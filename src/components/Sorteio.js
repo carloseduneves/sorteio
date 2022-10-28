14893116c7cf04e43d89ea63fbf6c45002e3420d
carloseduneves <carlosneves.edu@gmail.com>
2022-10-27 21:30:31 -0300

    Sorteio 1.0.1

--- src/components/Sorteio.js
+++ src/components/Sorteio.js
@@ -1,9 +1,40 @@
-
+import {useState, useRef, useEffect} from 'react';
 
 function Sorteio() {
+
+  let [nameList, setNameList] = useState([])
+  const [nameInput, setNameInput] = useState("");
+
+  const reference = useRef(null);
+
+  useEffect(() => {
+
+    e.preventDefault();
+
+    setNameList([...nameInput, nameInput])
+    console.log(nameList);
+    console.log(nameInput);
+  
+    reference.current.focus();
+    
+    setNameInput("");
+  })
+  const raffle = () => {
+    console.log(nameList);
+  }
   return (
     <div>
         <h1>Gerador de sorteios</h1>
+        <div className='window'>
+            <form onSubmit={useEffect}>
+              <span>Sorteio de nomes</span>
+              <input type="text" onChange={(e) => setNameInput(e.target.value)} ref={reference} value={nameInput}/>
+              <button>inserir nome</button>
+              
+              {nameList.map((itens) => <li>{itens}</li>)}
+            </form>
+            <button onClick={raffle}>sortear</button>
+        </div>
     </div>
   )
 }
